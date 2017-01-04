de535071f868e6558a5f5460ed246c7d01bb0ac6
vicmator <vimafelo@hotmail.es>
2017-01-04 14:46:07 +0100

    First commit works app with basic component

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component(
- {
-   selector: 'app', //This property is used to define how to call this component from HTML
-   templateUrl: './app.component.html' 
-}
-)
+{
+    selector: 'app', //This property is used to define how to call this component from HTML
+    template: '<h1>{{title}}</h1>' 
+    // templateUrl: 'app.component.html'
+})
+
 class App {
  title: string = "Hello Angular 2 with Webpack!"
 }
