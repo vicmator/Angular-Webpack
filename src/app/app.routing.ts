f7a83a57cbb6fb87db807fee4c3d479aede7d0bd
Victor Manuel Fernandez Lopez <vimafelo@hotmail.es>
2017-01-18 23:35:05 +0100

    Comment in routing to works component without errors

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,19 +3,19 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 
 const appRoutes :Routes = [
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  }
+  // {
+  //   path: '',
+  //   redirectTo: '/home',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'home',
+  //   component: HomeComponent
+  // },
+  // {
+  //   path: 'about',
+  //   component: AboutComponent
+  // }
 ]
 
 export const routing = RouterModule.forRoot(appRoutes);
