a1c101e542281e4a119a0ea963b0a5ed28f795ae
Victor Manuel Fernandez Lopez <vimafelo@hotmail.es>
2017-01-26 23:11:50 +0100

    Pass api to table.component

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,20 +1,14 @@
-import { BetModel } from './../../../model/betModel';
-import { BetAPI } from '../../../api/betApi';
 import { Component } from '@angular/core';
 
 @Component(
 {
   selector: 'header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  providers: [BetAPI]
+  styleUrls: ['./header.component.scss']
 })
 
 class HeaderComponent {
-  betsModel: BetModel[];
-  constructor(betApi: BetAPI) {
-    this.betsModel = betApi.getAllBetsJson();
-  }
+
   ngOnInit() {
   }
 }
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BetAPI } from '../../api/betApi';
+import { BetModel } from './../../model/betModel';
 
 @Component(
 {
   selector: 'table-bet',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
+  providers: [BetAPI]
 })
 
-class TableComponent {
+class TableComponent implements OnInit{
+  betsModel: BetModel[];
+
+  constructor(betApi: BetAPI) {
+    this.betsModel = betApi.getAllBetsJson();
+  }
   ngOnInit() {
 
   }
