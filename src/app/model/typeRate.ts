d6c741244e0320022313f3b67aea3f9c496ff3bd
Víctor Fernández López <vfernandez@solbooking.local>
2017-03-13 10:02:45 +0100

    #27 Add winRate to model

--- src/app/model/typeRate.ts
+++ src/app/model/typeRate.ts
@@ -1,11 +1,13 @@
 export class TypeRate {
   type: string;
   rate: number;
+  winRate: number;
   selected:boolean;
 
   constructor() {
     this.type = "";
     this.rate = 0;
+    this.winRate = 0;
     this.selected = false;
   }
 }
